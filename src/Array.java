6135fb56aa5737e77df133a2a1e20e2e7ea7cc87
Hrithik Gorane <hrithikgorane@gmail.com>
2024-05-21 20:55:29 +0530

    added something

--- src/Array.java
+++ src/Array.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+import java.sql.*;
 public class Array {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        String url ="";
+        String Name = "root";
+        String pass="";
+         Class.forName("com.mongodb.jdbc.java.sql.Driver");
+         Connection con= DriverManager.getConnection(url,Name,pass);
 
     }
 }
+/*
+1.import
+2.load&register driver
+3.cerate conncetion
+4.create statement
+5.excuteion querry
+6.procsees restult
+7.close
+ */
