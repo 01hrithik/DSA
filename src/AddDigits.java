6266e3d9838842f3e4f04b8018c6ff1d5ab29fc5
Hrithik Gorane <hrithikgorane@gmail.com>
2024-05-22 13:53:27 +0530

    solve AddDigit question

---
+++ src/AddDigits.java
@@ -0,0 +1,42 @@
+/*
+Given an integer num, repeatedly add all its digits until
+the result has only one digit, and return it.
+
+Example 1:
+
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2
+Since 2 has only one digit, return it.
+
+Example 2:
+
+Input: num = 0
+Output: 0
+ */
+
+
+import java.util.Scanner;
+
+public class AddDigits {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        while(num>=10){
+            int sum =0;
+
+            for(int j=num; j>=1; j/=10){
+                sum+=j%10;
+
+
+            }
+            num=sum;
+        }
+        System.out.println(num);
+
+    }
+}
+
