a941b6d90da4d440964f0728d21f71b48fd7c86e
Hrithik Gorane <hrithikgorane@gmail.com>
2024-05-27 20:14:27 +0530

    fun prime.

--- src/IsPrimeNumber.java
+++ src/IsPrimeNumber.java
@@ -1,5 +1,16 @@
 import java.util.Scanner;
+
 public class IsPrimeNumber {
+    public static int prime(int value){
+        for(int i=2; i<value-1; i++){
+
+            if(value % i == 0){
+                return  0;
+
+            }
+        }
+         return 1;
+    }
     public static void main(String[] args) {
         //taking input from user.
         Scanner sc=new Scanner(System.in);
@@ -14,5 +25,6 @@ public class IsPrimeNumber {
             }
         }
         System.out.println(isprime);
+        System.out.println(prime(15));
     }
 }
